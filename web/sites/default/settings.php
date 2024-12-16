29880d23bd5568a9b59fafd6511b487809717a21
Kevin Reynen <kevin.reynen@du.edu>
2024-12-16 13:53:48 -0700

    addded color coding for environment_indicator and fixed bug in update hook trying to set claro before it was enabled

--- web/sites/default/settings.php
+++ web/sites/default/settings.php
@@ -16,6 +16,50 @@ $settings['container_yamls'][] = __DIR__ . '/services.yml';
  */
 include __DIR__ . "/settings.pantheon.php";
 
+// Set Environment specific variables
+if (!defined('PANTHEON_ENVIRONMENT') && php_sapi_name() != 'cli') {
+  $config['environment_indicator.indicator']['name'] = 'Local';
+  $config['environment_indicator.indicator']['bg_color'] = '#4C742C';
+  $config['environment_indicator.indicator']['fg_color'] = '#ffffff';
+  $config['pantheon_domain_masking.settings']['enabled'] = 'no';
+}
+
+/**
+  * Pantheon settings should only be included when the site is running on Pantheon 
+  */
+if (isset($_ENV['PANTHEON_ENVIRONMENT']) && php_sapi_name() != 'cli') {
+  
+  // Add any additional Pantheon specific customizations bellow
+  if ($_ENV['PANTHEON_ENVIRONMENT'] != 'live') {
+    $config['pantheon_domain_masking.settings']['enabled'] = 'no';
+  }
+  // Add color and label to Admin Toolbar
+  switch ($_ENV['PANTHEON_ENVIRONMENT']) {
+    case 'dev':
+      $config['environment_indicator.indicator']['name'] = 'Dev';
+      $config['environment_indicator.indicator']['bg_color'] = '#d25e0f';
+      $config['environment_indicator.indicator']['fg_color'] = '#ffffff';
+      break;
+    case 'test':
+      $config['environment_indicator.indicator']['name'] = 'Test';
+      $config['environment_indicator.indicator']['bg_color'] = '#c50707';
+      $config['environment_indicator.indicator']['fg_color'] = '#ffffff';
+      break;
+    case 'live':
+      $config['environment_indicator.indicator']['name'] = 'Live';
+      $config['environment_indicator.indicator']['bg_color'] = '#000000';
+      $config['environment_indicator.indicator']['fg_color'] = '#ffffff';
+      break;
+    default:
+      // Multidev catchall
+      $config['environment_indicator.indicator']['name'] = 'Multidev';
+      $config['environment_indicator.indicator']['bg_color'] = '#efd01b';
+      $config['environment_indicator.indicator']['fg_color'] = '#000000';
+      break;
+  }
+}
+
+
 /**
  * Skipping permissions hardening will make scaffolding
  * work better, but will also raise a warning when you
